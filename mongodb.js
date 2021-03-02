1f1b605b0554383b8f5a273f7d192318df0a4a7b
Preet Shah <shahpreetk@gmail.com>
2021-03-02 15:46:34 +0530

    callback for insertOne method

--- mongodb.js
+++ mongodb.js
@@ -16,9 +16,18 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Preet",
-      age: 21,
-    });
+    db.collection("users").insertOne(
+      {
+        name: "Preet",
+        age: 21,
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert user");
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
