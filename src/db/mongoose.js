35b580ce0da8e50beee89b610b91d13bcdf08b45
Preet Shah <shahpreetk@gmail.com>
2021-03-06 14:36:33 +0530

    created mongoose models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,3 +4,47 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+});
+
+const task = new Task({
+  description: "Learn Mongoose",
+  completed: false,
+});
+
+task
+  .save()
+  .then(() => {
+    console.log(task);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+// const me = new User({
+//   name: "Preet",
+//   age: 21,
+// });
+
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log("Error", error);
+//   });
