6bb7c34ceba8986fb441262c7683387dc719e932
Preet Shah <shahpreetk@gmail.com>
2021-03-08 21:46:59 +0530

    updating user info possible

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
 const Task = require("./models/task");
+const { default: validator } = require("validator");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -39,6 +40,33 @@ app.get("/users/:id", async (req, res) => {
     res.send(user);
   } catch {
     (e) => res.status(500).send();
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
   }
 });
 
