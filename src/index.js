48d6580e31e1db452dae73d3ab58d3de5232682d
Preet Shah <shahpreetk@gmail.com>
2021-03-08 15:59:51 +0530

    api made shifted to asynv await

--- src/index.js
+++ src/index.js
@@ -8,39 +8,36 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
-
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch {
+    res.status(400).send(e);
+  }
 });
 
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((e) => res.status(500).send());
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (e) {}
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
 
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
+  try {
+    const user = await User.findById(_id);
 
-      res.send(user);
-    })
-    .catch((e) => res.status(500).send());
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch {
+    (e) => res.status(500).send();
+  }
 });
 
 app.post("/tasks", (req, res) => {
