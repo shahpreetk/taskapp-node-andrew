719a9e820261c056ed388ad7d702f1f4333b7ee7
Preet Shah <shahpreetk@gmail.com>
2021-03-08 23:30:13 +0530

    delete task

--- src/index.js
+++ src/index.js
@@ -145,6 +145,19 @@ app.patch("/tasks/:id", async (req, res) => {
   }
 });
 
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 app.listen(port, () => {
   console.log("Server running on port " + port);
 });
