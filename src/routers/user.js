262fc03a35c78e439f089c604ec7edf1a2988bf8
Preet Shah <shahpreetk@gmail.com>
2021-03-09 11:31:14 +0530

    jwt auth tokens generated for login and signup

--- src/routers/user.js
+++ src/routers/user.js
@@ -6,7 +6,8 @@ router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -18,8 +19,9 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
+    const token = await user.generateAuthToken();
 
-    res.send(user);
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send();
   }
