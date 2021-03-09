a9787baed2119ea654c194ca1c2be7caac4e74e0
Preet Shah <shahpreetk@gmail.com>
2021-03-09 16:34:19 +0530

    auth middleware done

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 const User = require("../models/user");
 
@@ -27,13 +28,8 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send;
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
