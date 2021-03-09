b7577ab84df4b5e86568ff0781ccbc3663be47ca
Preet Shah <shahpreetk@gmail.com>
2021-03-09 10:38:28 +0530

    hashing passwords and refactoring update user and task

--- src/routers/user.js
+++ src/routers/user.js
@@ -7,7 +7,7 @@ router.post("/users", async (req, res) => {
   try {
     await user.save();
     res.status(201).send(user);
-  } catch {
+  } catch (e) {
     res.status(400).send(e);
   }
 });
@@ -48,10 +48,17 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
+    // const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    //   new: true,
+    //   runValidators: true,
+    // });
+
+    const user = await User.findById(req.params.id);
+    updates.forEach((update) => {
+      user[update] = req.body[update];
     });
+
+    await user.save();
 
     if (!user) {
       return res.status(404).send();
