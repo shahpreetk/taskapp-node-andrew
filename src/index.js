1365ae7696fec5ca8433915081730e1324818677
Preet Shah <shahpreetk@gmail.com>
2021-03-09 11:43:21 +0530

    site under maintenance middleware

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("Get requests are diabled");
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use((req, res, next) => {
+//   res.status(503).send("Site is currently down. Check back soon!");
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
