06f88e8fbd395186b842eb2990c5452bfc4823d0
Preet Shah <shahpreetk@gmail.com>
2021-03-10 13:07:05 +0530

    pagination done

--- src/routers/task.js
+++ src/routers/task.js
@@ -29,6 +29,10 @@ router.get("/tasks", auth, async (req, res) => {
       .populate({
         path: "tasks",
         match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+        },
       })
       .execPopulate();
     res.send(req.user.tasks);
