51e6ce09ec9921a691b30db4a4dc86f21db27847
Preet Shah <shahpreetk@gmail.com>
2021-03-12 18:33:21 +0530

    test for read profile

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,12 +1,21 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
+  _id: userOneId,
   name: "Fluff",
   email: "fluff@email.com",
   password: "fluffybunny",
   age: 5,
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
 };
 
 beforeEach(async () => {
@@ -44,4 +53,16 @@ test("Should not login non-existing user", async () => {
       password: "rahuladiti!!",
     })
     .expect(400);
+});
+
+test("Should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Should not get profile for unauthenticated user", async () => {
+  await request(app).get("/users/me").send().expect(401);
 });
