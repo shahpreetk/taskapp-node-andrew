2643bf4fbe33d4c38ee52c40a1d6982480c77351
Preet Shah <shahpreetk@gmail.com>
2021-03-12 18:07:28 +0530

    test for logging in

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,8 +2,16 @@ const request = require("supertest");
 const app = require("../src/app");
 const User = require("../src/models/user");
 
+const userOne = {
+  name: "Fluff",
+  email: "fluff@email.com",
+  password: "fluffybunny",
+  age: 5,
+};
+
 beforeEach(async () => {
   await User.deleteMany();
+  await new User(userOne).save();
 });
 
 test("Should signup a new user", async () => {
@@ -16,4 +24,24 @@ test("Should signup a new user", async () => {
       age: 5,
     })
     .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
+
+test("Should not login non-existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: "rahul@email.com",
+      password: "rahuladiti!!",
+    })
+    .expect(400);
 });
